a212966b231e2452022d640d01b220e346eef499
Abhijit <abhijitpatra30@gmail.com>
2023-04-20 12:24:56 +0530

    nav bar

--- src/app/components/sidebar/navList.ts
+++ src/app/components/sidebar/navList.ts
@@ -3,13 +3,15 @@ export const items = [
       text: "Inbox",
       icon: "k-i-inbox",
       selected: true,
+      path:'/pages/dashbord'
     },
     {
       separator: true,
     },
     {
       text: "Attachments",
-      icon: "k-i-envelop-link"
+      icon: "k-i-envelop-link",
+      path:'/pages/addrecord'
     },
     {
       separator: true,
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -5,6 +5,7 @@ import {
   DrawerSelectEvent,
 } from "@progress/kendo-angular-layout";
 import {items} from './navList';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -16,20 +17,26 @@ export class SidebarComponent {
   public selected = "Inbox";
   public expandedIndices = [2];
   public items: Array<DrawerItem> = items;
-
+  constructor(private route : Router){};
+  
   public isItemExpanded: DrawerItemExpandedFn = (item): boolean => {
     return this.expandedIndices.indexOf(item.id) >= 0;
   };
-  public onSelect(ev: DrawerSelectEvent): void {
-    this.selected = ev.item.text;
-    const current = ev.item.id;
 
-    if (this.expandedIndices.indexOf(current) >= 0) {
-      this.expandedIndices = this.expandedIndices.filter(
-        (id) => id !== current
-      );
-    } else {
-      this.expandedIndices.push(current);
-    }
+  public onSelect(ev: DrawerSelectEvent): void {
+    // this.selected = ev.item.text;
+    // const current = ev.item.id;
+    // console.log(ev.item.path);
+    this.route.navigate([ev.item.path])
+
+    //for child routes
+
+    // if (this.expandedIndices.indexOf(current) >= 0) {
+    //   this.expandedIndices = this.expandedIndices.filter(
+    //     (id) => id !== current
+    //   );
+    // } else {
+    //   this.expandedIndices.push(current);
+    // } 
   }
 }
